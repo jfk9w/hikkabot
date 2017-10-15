461817570027af7c0e4b883965ee0ee29194445b
jfk9w <xp.jfk9w@gmail.com>
2017-10-15 23:08:23 +0300

    scripts

---
+++ bot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BUILD=build
+APP=$BUILD/app
+PID=$BUILD/.pid
+LOG=$BUILD/log
+SNAPSHOT=$BUILD/snapshot
+
+CMD=$1
+case "$CMD" in
+
+	"build")
+		go build -o $APP -v
+		;;
+
+	"start")
+		if [ -f $PID ]; then
+			echo "Already running"
+		else
+			$APP -token=$2 -snapshot=$SNAPSHOT -log=debug > $LOG 2>&1 &
+			echo $! > $PID
+		fi
+		;;
+
+	"stop")
+		if [ -f $PID ]; then
+			kill `cat $PID`
+			rm $PID
+		else
+			echo "Not running"
+		fi
+		;;
+		
+esac
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./tele2ch -token=$1 -log=debug 2>&1
