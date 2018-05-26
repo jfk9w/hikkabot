61cc75ddcf2aedde9a7ea0c51cf1fba41f82ff35
jfk9w <xp.jfk9w@gmail.com>
2018-05-26 13:39:01 +0300

    minor fix

--- text/post.go
+++ text/post.go
@@ -14,6 +14,10 @@ type Post struct {
 
 func FormatPost(post Post) []string {
 	chunks := format(post.Item, telegram.MaxMessageSize*4/5)
+	if len(chunks) == 0 {
+		chunks = []string{""}
+	}
+
 	if len(chunks) > 0 {
 		chunks[0] = fmt.Sprintf("%s\n%s\n---\n%s", post.Hashtag, FormatRef(post.Ref), chunks[0])
 		if post.Parent == 0 {
