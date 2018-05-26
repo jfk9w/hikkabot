ed23939a31c2a0f15fe46c3902775fb00d8cf06e
jfk9w <xp.jfk9w@gmail.com>
2018-05-26 21:35:07 +0300

    more limit subject hashtag

--- text/format.go
+++ text/format.go
@@ -24,7 +24,7 @@ var hashtagScreen = regexp.MustCompile("[^0-9A-Za-zА-Яа-я]+")
 
 func FormatSubject(subject string) Hashtag {
 	str := "#" + hashtagScreen.ReplaceAllString(subject, "_")
-	str = misc.FirstRunes(str, 27, "")
+	str = misc.FirstRunes(str, 25, "")
 	last := misc.FindLastRune(str, '_', 0, 0)
 	if last > 0 && misc.RuneLength(str)-last < 2 {
 		str = misc.SliceRunes(str, 0, last)
