8a72ae5cbd5b91a9190ace55c2cf880e5ce1b203
jfk9w <xp.jfk9w@gmail.com>
2018-05-27 14:52:59 +0300

    fixes

--- main.go
+++ main.go
@@ -19,6 +19,30 @@ import (
 
 var log = logrus.GetLogger("main")
 
+type Config struct {
+	BackendGCTimeout    int `json:"backend_gc_timeout"`
+	AconvertReadTimeout int `json:"aconvert_read_timeout"`
+
+	Keeper   keeper.Config   `json:"keeper"`
+	Telegram telegram.Config `json:"telegram"`
+	Dvach    dvach.Config    `json:"dvach"`
+	Aconvert aconvert.Config `json:"aconvert"`
+}
+
+func readConfig() *Config {
+	path := os.Getenv("CONFIG")
+	if path == "" {
+		panic("CONFIG not set")
+	}
+
+	cfg := new(Config)
+	if err := misc.ReadJSON(path, cfg); err != nil {
+		panic(err)
+	}
+
+	return cfg
+}
+
 func main() {
 	defer func() {
 		if err := recover(); err != nil {
