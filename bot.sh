29762fe14060bcd868a6ecfdd7450721af50e547
jfk9w <xp.jfk9w@gmail.com>
2018-05-30 13:08:28 +0300

    fix

--- bot.sh
+++ bot.sh
@@ -1,113 +1,185 @@
 #!/bin/bash
 
-PATH=$PATH:$HOME/Go/bin
+RED=31
+GREEN=32
 
-CONFIG=$HOME/.config/hikkabot.json
-RUNFILE=$HOME/.hikkabot
-LOGDIR=$HOME/hikkabot/logs
-PACKAGE=github.com/jfk9w-go/hikkabot
-
-archive_logs() {
-    CWD=`pwd`
-    DIR=`date +%Y-%m-%d_%H-%M-%S`
-    cd ${LOGDIR}
-    mkdir ${DIR}
-    mv *.log ${DIR}
-    cd ${CWD}
+function color() {
+    printf "\x1b[$2m$1\x1b[0m"
 }
 
-start() {
-    if [[ -f ${RUNFILE} ]]; then
-        echo "Hikkabot instance already running, PID: `cat ${RUNFILE}`"
-    else
-        mkdir -p ${LOGDIR}
-        env CONFIG=${CONFIG} LOG=${CONFIG} hikkabot 2>&1 > ${LOGDIR}/main.log &
-        echo -e "PID=$!" > ${RUNFILE}
-        notify "RUNNING" 1
-    fi
+function mute() {
+    $@ > /dev/null 2>&1
 }
 
-stop() {
-    if [[ -f ${RUNFILE} ]]; then
-        source ${RUNFILE}
-        kill ${PID}
-        if [[ $? -eq 0 ]]; then
-            notify "SHUTDOWN" 1
-            echo "Waiting for Hikkabot instance death, PID: ${PID}"
-            tail -f ${LOGDIR}/main.log | while read LOGLINE; do
-                [[ "${LOGLINE}" == *"[main] Exit"* ]] && pkill -P $$ tail
-            done
+function prev() {
+    if [[ $? -ne 0 ]]; then
+        if [[ "$1" != "" ]]; then
+            echo "$(color ERROR "$RED") $1"
         fi
-        rm ${RUNFILE}
-        archive_logs
-        echo "OK"
-    else
-        echo "Hikkabot instance not running"
+
+        exit 1
     fi
 }
 
-notify() {
-    TEXT=`echo $1 | sed -r 's/\s+/%20/g;s/\./%2E/g'`
-    NOTIFY=$2
-    TOKEN=`cat ${CONFIG} | jq -r ".telegram | .token"`
-    CHAT=`cat ${CONFIG} | jq -r ".mgmt"`
-    FORM="chat_id=${CHAT}&text=${TEXT}"
-    if [[ ! ${NOTIFY} ]]; then
-        FORM="${FORM}&disable_notification=true"
-    fi
-
-    curl -s -d ${FORM} -X POST https://api.telegram.org/bot${TOKEN}/sendMessage > /dev/null
+function require() {
+    mute type $1
+    prev "$1 not found"
 }
 
-check() {
-    if [[ -f ${RUNFILE} ]]; then
-        source ${RUNFILE}
-        kill -0 ${PID}
-        if [[ $? -ne 0 ]]; then
-            rm ${RUNFILE}
-            notify "Instance is not running. Restarting." 1
-            archive_logs
-            start
-            sleep 5
-            check
-#        else
-#            STATS=`ps -p ${PID} -o %cpu,%mem | tail -1`
-#            notify ${CONFIG} ${CHAT} "${STATS}"
+SED=sed
+if [[ $(uname) == "Darwin" ]]; then
+    SED=gsed
+fi
+
+require go
+require jq
+require curl
+require "$SED"
+
+if [[ "$GOPATH" == "" ]]; then
+    printf "GOPATH is not set"
+    exit 1
+fi
+
+PATH="$PATH:$GOPATH/bin"
+PACKAGE="github.com/jfk9w-go/hikkabot"
+
+if [[ "$RUNFILE" == "" ]]; then
+    PIDFILE="$HOME/.hikkabot"
+fi
+
+if [[ "$CONFIG" == "" ]]; then
+    CONFIG="`pwd`/config.json"
+fi
+
+if [[ "$STDOUT" == "" ]]; then
+    STDOUT="`pwd`/hikkabot.run"
+fi
+
+printf "Package: $PACKAGE\nPidfile: $PIDFILE\nConfig path: $CONFIG\nStdout: $STDOUT\n"
+
+cat "$CONFIG" | jq -e . > /dev/null
+prev
+
+function config() {
+    cat "$CONFIG" | jq -r "$1"
+}
+
+test $(config ".mgmt") != ""
+prev "Config mgmt field not found"
+
+test $(config ".telegram | .token") != ""
+prev "Config token field not found"
+
+function start() {
+    if [[ -f "$PIDFILE" ]]; then
+        mute . "$PIDFILE"
+        if [[ $? -ne 0 || "$PID" == "" ]]; then
+            echo "PID not set"
+            exit 1
         fi
+
+        echo "Hikkabot instance already running, PID: $PID"
+        exit 2
+    fi
+
+    mkdir -p "$(dirname "$STDOUT")"
+    CONFIG="$CONFIG" LOG="$CONFIG" hikkabot 2>&1 > "$STDOUT" &
+    prev "Failed to start hikkabot"
+
+    PID=$!
+    echo -e "PID=$PID" > "$PIDFILE"
+    echo "PID: $PID"
+    notify "RUNNING" 1
+}
+
+function stop() {
+    if [[ ! -f "$PIDFILE" ]]; then
+        echo "Pidfile not found"
+        exit 2
+    fi
+
+    mute . "$PIDFILE"
+    if [[ $? -ne 0 || "$PID" == "" ]]; then
+        echo "PID not set"
+        exit 1
     else
-        notify "Runfile not found." 1
+        echo "PID: $PID"
+    fi
+
+    mute kill "$PID"
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to kill process, cleanup"
+        cleanup
+        exit 2
+    fi
+
+    notify "SHUTDOWN" 1
+    echo "Waiting for Hikkabot instance death, PID: $PID"
+    tail -f "$STDOUT" | while read LOGLINE; do
+        [[ "$LOGLINE" == *"[main] Exit"* ]] && pkill -P $$ tail
+    done
+
+    cleanup
+    echo "OK"
+}
+
+function cleanup() {
+    rm "$PIDFILE"
+}
+
+function notify() {
+    TEXT=$(echo "$1" | "$SED" -r 's/\s+/%20/g;s/\./%2E/g')
+    FORM="chat_id=$(config ".mgmt")&text=$TEXT"
+    if [[ ! $2 ]]; then
+        FORM="$FORM&disable_notification=true"
+    fi
+
+    mute curl -s -d "$FORM" -X POST "https://api.telegram.org/bot$(config ".telegram | .token")/sendMessage"
+}
+
+function check() {
+    if [[ ! -f "$PIDFILE" ]]; then
+        notify "Pidfile not found" 1
+        exit 0
+    fi
+
+    mute . "$PIDFILE"
+    if [[ $? -ne 0 || "$PID" == "" ]]; then
+        echo "PID not set"
+        exit 1
+    else
+        echo "PID: $PID"
+    fi
+
+    mute kill -0 "$PID"
+    if [[ $? -ne 0 ]]; then
+        cleanup
+        notify "RESTART" 1
+        start
+        sleep 5
+        check
     fi
 }
 
-install() {
-    go get -v -u ${PACKAGE}
-    go install ${PACKAGE}
-    cp ${GOPATH}/src/${PACKAGE}/bot.sh .
+function install() {
+    go get -v -u "$PACKAGE"
+    prev
+
+    go install -v "$PACKAGE"
+    prev
+
+    cp "$GOPATH/src/$PACKAGE/bot.sh" .
+    prev
 }
 
-restart() {
+function restart() {
     stop
+    if [[ $? -ne 0 ]]; then
+        exit 1
+    fi
+
     start
 }
 
-case $1 in
-    "start")
-        start
-        ;;
-
-    "stop")
-        stop
-        ;;
-
-    "check")
-        check
-        ;;
-
-    "install")
-        install
-        ;;
-
-    "restart")
-        restart
-        ;;
-esac
+"$@"
