a825e5ec2ebb25990e191bd2bc0320cacda5e828
jfk9w <xp.jfk9w@gmail.com>
2019-08-24 22:58:10 +0300

    fix deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TMP=/tmp/hikkabot-deploy
-BIN=hikkabot
-WD=/home/ryan/apps/hikkabot
-GOOS=linux GOARCH=amd64 go build -o bin/$BIN
-ssh root@vps "rm -r $TMP; mkdir $TMP"
-scp bin/hikkabot root@vps:/tmp/hikkabot-deploy
-ssh root@vps "supervisorctl stop hikkabot; mv $TMP/$BIN $WD; supervisorctl start hikkabot"
---
+++ infra/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+GOOS=linux GOARCH=amd64 go build -o ../bin/hikkabot
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+ansible-playbook "$DIR/deploy.yml"
